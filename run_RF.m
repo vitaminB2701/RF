6584ed5cb2f27713d5d72ae68696140dc36afa57
vitaminB2701 <longtrumdaugau1997@gmail.com>
2022-12-14 18:11:52 +0100

    add comments to run_RF

--- run_RF.m
+++ run_RF.m
@@ -8,16 +8,16 @@ clear; %close all
 
 %% Set-up
 % Parameters
-Par.X = 14000:20:16000; % Wavenumber scale
+Par.X = 14000:20:16000; % Wavenumber scale [cm-1]
 Par.T = 80;         % Temperature [K]
 Par.Vc = 20;        % Coupling cutoff (for clustering)
 Par.Ec = 300;       % Energy difference cutoff (for clustering)
 Par.Rc = 0.5;       % Protein correlation radius [nm]
 Par.t = logspace(-2,3,150);  % Population time (for kinetics) [ps]
-Par.exc = 14400:20:16000;    % Excitation frequency (for kinetics)
+Par.exc = 14400:20:16000;    % Excitation frequency (for kinetics) [cm-1]
 Par.BlockSize = 4;  % Number of parallel runs in a block, for static disorder
 Par.Niter = 25; % Number of blocks to run (total=BlockSize*Niter)
-Par.taudeph = 0.3; % Pure dephasing time (ps)
+Par.taudeph = 0.30; % Pure dephasing time (ps)
 
 % Output file
 fileout = "RF_out"+'_'+datestr(now,'yyyymmdd_HHMMss')+'.mat';
