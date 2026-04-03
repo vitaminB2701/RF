cc9d5257b95fb5922e76764bc7d9357ba7ba48bf
HLong <longtrumdaugau1997@gmail.com>
2026-04-03 16:53:19 +0800

    Add plot funciton to run_RF.m

--- run_RF.m
+++ run_RF.m
@@ -1,24 +1,4 @@
-% Redfield-Forster model
-%
-% Input
-% 1) PDB file containing coordinates of Mg, Nb and Nd atoms for each Chl
-% 2) Excel table with site energies
-
-% Static disorder is simulated by repeating the calculation with Gaussian
-% randomized site energies. The sampling is divided into blocks, where
-% after each block is done calculating, the result is averaged with
-% previous blocks and saved. The process is iterated so that the total
-% randomizations samples is Nsample=BlockSize*Niteration. 
-% By this way, the process of sampling over static disorder can be
-% constantly recorded throughout the calculation. The program can be 
-% terminated at anytime during the sampling process without losing the 
-% already simulated results.
-% However, a portion of calculation time will be sacrificed to write data.
-% If desired, the number of iteration (Niter) can be reduced while
-% increasing the BlockSize to reduced the writing time.
-% Generally, a parallel run does not improve the performance of
-% this code.
-
+% Run Redfield-Forster model
 clear; %close all
 
 %% Load input
@@ -41,3 +21,6 @@ end
 
 %% Run calculation
 RF = redfield_foerster(atom,Epar,Par,fileout);
+
+%% Plot results
+plot_RF;
